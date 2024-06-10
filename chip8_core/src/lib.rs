dc67ec40a65149b5681802aa6b54f3dae3a5b595
Michael Xie <michaelx.10@hotmail.com>
2024-06-09 22:49:03 -0400

    finished the emulator!

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -59,144 +59,35 @@ impl Emu {
         self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
     }
 
-    fn push(&mut self, val: u16) {
-        self.stack[self.sp as usize] = val;
-        self.sp += 1;
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
     }
 
-    fn pop(&mut self) -> u16 {
-        self.sp -= 1; // Note, might pop empty stack but emulator/game code should avoid this issue
-                      // TODO May want to add handling on this feature
-        self.stack[self.sp as usize] // Returns what is on top of the new stack
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        self.keys[idx] = pressed;
     }
 
-    pub fn tick(&mut self){
-        // Fetch value of game from RAM
-        let op = self.fetch();
-        // Decode and execute
-        self.execute(op);
+    // Loads the game code from RAM to be executed
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+        self.ram[start..end].copy_from_slice(data);
     }
 
-    fn execute(&mut self, op: u16){
-        let digit1 = (op & 0xF000) >> 12;
-        let digit2 = (op & 0x0F00) >> 8;
-        let digit3 = (op & 0x00F0) >> 4;
-        let digit4 = op & 0x000F;
-        
-        // Implementation of opcodes
-        match (digit1, digit2, digit3, digit4){
-            // NOP
-            (0, 0, 0, 0) => return,
-
-            //Clear Screen
-            (0, 0, 0xE, 0) => {
-                self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
-            },
-
-            // Return from Subroutine/Functions
-            // Push off address where function is run off the stack
-            (0, 0, 0xE, 0xE) =>{
-                let ret_addr = self.pop();
-                self.pc = ret_addr;
-            },
-
-            // Jump to given address
-            // 1NNN
-            (1, _, _, _)  => {
-                self.pc = op & 0xFFF;
-            },
-
-            // Call subroutine/function
-            (2, _, _, _)=>{
-                let nnn = op & 0xFFF;
-                self.push(self.pc);
-                self.pc = nnn;
-            },
-
-            // 3XNN, skip next if VX == NN
-            // X is what register to use, NN is raw value
-            // Remember V registers are registers a game/program can use
-            (3, _, _, _) =>{
-                let x = digit2 as usize;
-                let nn = (op & 0xFF) as u8;
-                if self.v_reg[x] == nn{
-                    self.pc += 2; // SKip instruction
-                }
-            },
-
-            // 4XNN, skip next if VX != NN
-
-            (4, _, _, _) =>{
-                let x = digit2 as usize;
-                let nn = (op & 0xFF) as u8;
-                if self.v_reg[x] != nn{
-                    self.pc += 2;
-                }
-            },
-
-            // 5XY0, use third digit to check if VX == VY
-
-            (5, _, _, 0) =>{
-                let x = digit2 as usize;
-                let y = digit3 as usize;
-                if self.v_reg[x] == self.v_reg[y]{
-                    self.pc == 2;
-                }
-            },
-
-            // 9XY0, skip if VX != VY
-            (9, _, _, 0) =>{
-                let x = digit2 as usize;
-                let y = digit3 as usize;
-                if self.v_reg[x] != self.v_reg[y]{
-                    self.pc += 2;
-                }
-            },
-
-            // Use I Register, address pointer to RAM
-            // ANNN - I = NNN, set set the address pointer to NNN
-
-            (0xA, _, _, _) => {
-                let nnn = op & 0xFFF;
-                self.i_reg = nnn;
-            },
-
-            
-            // BNNN, uses V0 register move PC = V0 + value in opcode
-
-            (0xB, _, _, _) =>{
-                self.pc = (self.v_reg[0] as u16) + op & 0xFFF;
-            }
-
-            // CXNN - VX = rand() & NN
-            // Add RNG support
-            (0xC, _, _, _) =>{
-                let x = digit2 as usize;
-                let nn = (op & 0xFF) as u8;
-                let rng: u8 = random();
-                self.v_reg[x] = rng & nn;
-            }
-
-            // Unknown opcode
-            // _ means compiler must expect a value from this as it is going to interpret it
-            (_,_,_,_,) => unimplemented!("Unimplemented opcode: {}", op), // Demands that all options are taken care of, unimplemented! macro used to panic it it reaches that point
-        }
-    }
-
-    pub fn tick_timers(&mut self){
-        if self.dt > 0{
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
             self.dt -= 1;
         }
-        
-        if self.st > 0{
-            if self.st == 1{
+
+        if self.st > 0 {
+            if self.st == 1 {
                 // Beep
             }
             self.st -= 1;
         }
     }
 
-    fn fetch(&mut self) -> u16{
+    fn fetch(&mut self) -> u16 {
         // 35 opcodes to deal with with it being 16 bits
         let higher_byte = self.ram[self.pc as usize] as u16; // XOOO Extract MSB
         let lower_byte = self.ram[(self.pc + 1) as usize] as u16; // OOOX Extract LSB
@@ -220,5 +111,346 @@ impl Emu {
         };
         new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
         new_emu
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16 {
+        self.sp -= 1; // Note, might pop empty stack but emulator/game code should avoid this issue
+                      // TODO May want to add handling on this feature
+        self.stack[self.sp as usize] // Returns what is on top of the new stack
+    }
+
+    pub fn tick(&mut self) {
+        // Fetch value of game from RAM
+        let op = self.fetch();
+        // Decode and execute
+        self.execute(op);
+    }
+
+    fn execute(&mut self, op: u16) {
+        let digit1 = (op & 0xF000) >> 12;
+        let digit2 = (op & 0x0F00) >> 8;
+        let digit3 = (op & 0x00F0) >> 4;
+        let digit4 = op & 0x000F;
+
+        // Implementation of opcodes
+        match (digit1, digit2, digit3, digit4) {
+            // NOP
+            (0, 0, 0, 0) => return,
+
+            //Clear Screen
+            (0, 0, 0xE, 0) => {
+                self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+            }
+
+            // Return from Subroutine/Functions
+            // Push off address where function is run off the stack
+            (0, 0, 0xE, 0xE) => {
+                let ret_addr = self.pop();
+                self.pc = ret_addr;
+            }
+
+            // Jump to given address
+            // 1NNN
+            (1, _, _, _) => {
+                self.pc = op & 0xFFF;
+            }
+
+            // Call subroutine/function
+            (2, _, _, _) => {
+                let nnn = op & 0xFFF;
+                self.push(self.pc);
+                self.pc = nnn;
+            }
+
+            // 3XNN, skip next if VX == NN
+            // X is what register to use, NN is raw value
+            // Remember V registers are registers a game/program can use
+            (3, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                if self.v_reg[x] == nn {
+                    self.pc += 2; // SKip instruction
+                }
+            }
+
+            // 4XNN, skip next if VX != NN
+            (4, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                if self.v_reg[x] != nn {
+                    self.pc += 2;
+                }
+            }
+
+            // 5XY0, use third digit to check if VX == VY
+            (5, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                if self.v_reg[x] == self.v_reg[y] {
+                    self.pc += 2;
+                }
+            }
+
+            // TODO Work on 6 Registers
+            // Set the V register to the second digit to the value given
+            // VX = NN
+            (6, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                self.v_reg[x] = nn;
+            }
+
+            // Adds the third and fourth digit to the VX register
+
+            // VX += NN
+            (7, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                self.v_reg[x] = self.v_reg[x].wrapping_add(nn);
+            }
+
+            // VX = VY
+            (8, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] = self.v_reg[y];
+            }
+
+            // Implements or function, and, xor
+            (8, _, _, 1) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] |= self.v_reg[y];
+            }
+
+            (8, _, _, 2) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] &= self.v_reg[y];
+            }
+
+            (8, _, _, 3) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                self.v_reg[x] ^= self.v_reg[y];
+            }
+
+            // VX += VY
+            (8, _, _, 4) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, carry) = self.v_reg[x].overflowing_add(self.v_reg[y]);
+                let new_vf = if carry { 1 } else { 0 }; // If overflow occurs or not, set flag register
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = new_vf;
+            }
+
+            // VX -= VY
+            (8, _, _, 5) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, borrow) = self.v_reg[x].overflowing_sub(self.v_reg[y]);
+                let new_vf = if borrow { 0 } else { 1 };
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = new_vf;
+            }
+
+            // Single right shift on VX
+            // VX >>= 1
+            (8, _, _, 6) => {
+                let x = digit2 as usize;
+                let lsb = self.v_reg[x] & 1;
+                self.v_reg[x] >>= 1;
+                self.v_reg[0xF] = lsb; // Catches the dropped bit
+            }
+
+            // VX = VY - VX
+            (8, _, _, 7) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                let (new_vx, borrow) = self.v_reg[y].overflowing_sub(self.v_reg[x]);
+                let new_vf = if borrow { 0 } else { 1 };
+                self.v_reg[x] = new_vx;
+                self.v_reg[0xF] = new_vf;
+            }
+
+            // VX <<= 1
+            // Store value that is overflowed in flag
+            (8, _, _, 0xE) => {
+                let x = digit2 as usize;
+                let msb = (self.v_reg[x] >> 7) & 1;
+                self.v_reg[x] <<= 1;
+                self.v_reg[0xF] = msb;
+            }
+
+            // 9XY0, skip if VX != VY
+            (9, _, _, 0) => {
+                let x = digit2 as usize;
+                let y = digit3 as usize;
+                if self.v_reg[x] != self.v_reg[y] {
+                    self.pc += 2;
+                }
+            }
+
+            // Use I Register, address pointer to RAM
+            // ANNN - I = NNN, set set the address pointer to NNN
+            (0xA, _, _, _) => {
+                let nnn = op & 0xFFF;
+                self.i_reg = nnn;
+            }
+
+            // BNNN, uses V0 register move PC = V0 + value in opcode
+            (0xB, _, _, _) => {
+                self.pc = (self.v_reg[0] as u16) + op & 0xFFF;
+            }
+
+            // CXNN - VX = rand() & NN
+            // Add RNG support
+            (0xC, _, _, _) => {
+                let x = digit2 as usize;
+                let nn = (op & 0xFF) as u8;
+                let rng: u8 = random();
+                self.v_reg[x] = rng & nn;
+            }
+
+            // Graphics
+            // Chip-8 draws sprites at a specified (x, y)
+            // Second and third digits are V Registers, which we fetch (x, y) coordinates from
+            // Sprites width are always 8px, but the height are stored row by row from *I, *I+1...
+            // If pixel is flipped from white to black, the VF is set
+
+            // Draw
+            (0xD, _, _, _) => {
+                let x_coord = self.v_reg[digit2 as usize] as u16;
+                let y_coord = self.v_reg[digit3 as usize] as u16;
+                let num_rows = digit4;
+
+                // Keeps track if any pixels were flipped
+                let mut flipped = false;
+
+                for y_line in 0..num_rows {
+                    // Store memory address for row's data
+                    let addr = self.i_reg + y_line as u16;
+                    let pixels = self.ram[addr as usize];
+
+                    // Iterate through column
+                    for x_line in 0..8 {
+                        if (pixels & (0b1000_0000 >> x_line)) != 0 {
+                            let y = (y_coord + y_line) as usize % SCREEN_HEIGHT;
+                            let x = (x_coord + x_line) as usize % SCREEN_WIDTH;
+
+                            let idx = x + SCREEN_WIDTH * y;
+
+                            flipped |= self.screen[idx];
+                            self.screen[idx] ^= true;
+                        }
+                    }
+                }
+                // Populate VF Register
+
+                if flipped {
+                    self.v_reg[0xF] = 1;
+                } else {
+                    self.v_reg[0xF] = 0;
+                }
+            }
+
+            // User Input
+            // Checks if index stores in VX is pressed, if so skips the next instruction
+            (0xE, _, 9, 0xE) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if key {
+                    self.pc += 2;
+                }
+            }
+
+            // Skips if not being pressed
+            (0xE, _, 0xA, 1) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if !key {
+                    self.pc += 2;
+                }
+            }
+
+            (0xF, _, 0, 7) => {
+                let x = digit2 as usize;
+                self.v_reg[x] = self.dt;
+            }
+
+            // Stores the delay timer into one of the V Registers for use
+            (0xF, _, 1, 5) => {
+                let x = digit2 as usize;
+                self.dt = self.v_reg[x];
+            }
+
+            // Same for sound timer
+            (0xF, _, 1, 8) => {
+                let x = digit2 as usize;
+                self.st = self.v_reg[x];
+            }
+
+            // FX1E - I += VX
+            // Sets I to the encoded 0xNNN value
+            (0xF, _, 1, 0xE) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x] as u16;
+                self.i_reg = self.i_reg.wrapping_add(vx); // If overlfow, goes back to 0
+            }
+
+            //  Store font address into I register
+            (0xF, _, 2, 9) => {
+                let x = digit2 as usize;
+                let c = self.v_reg[x] as u16;
+                self.i_reg = 5 * c; // Point to that number in ram
+            }
+
+            // Convert hexadecimal/binary to Base 10 Number
+            (0xF, _, 3, 3) => {
+                let x = digit2 as usize;
+                let vx = self.v_reg[x] as f32;
+                // Fetch the hundreds digit by dividing by 100 and tossing the decimal
+                let hundreds = (vx / 100.0).floor() as u8;
+                // Fetch the tens digit by dividing by 10, tossing the ones digit and the decimal
+                let tens = ((vx / 10.0) % 10.0).floor() as u8;
+                // Fetch the ones digit by tossing the hundreds and the tens
+                let ones = (vx % 10.0) as u8;
+                self.ram[self.i_reg as usize] = hundreds;
+                self.ram[(self.i_reg + 1) as usize] = tens;
+                self.ram[(self.i_reg + 2) as usize] = ones;
+            }
+
+            // Store the values from V0- VX into I
+            (0xF, _, 5, 5) => {
+                let x = digit2 as usize;
+                let i = self.i_reg as usize;
+                for idx in 0..=x {
+                    self.ram[i + idx] = self.v_reg[idx];
+                }
+            }
+
+            // Load I into V0- VX
+            (0xF, _, 6, 5) => {
+                let x = digit2 as usize;
+                let i = self.i_reg as usize;
+                for idx in 0..=x {
+                    self.v_reg[idx] = self.ram[i + idx];
+                }
+            }
+
+            // TODO Cover opcodes 0x0000, 0xE000, 0xF000
+
+            // Unknown opcode
+            // _ means compiler must expect a value from this as it is going to interpret it
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op), // Demands that all options are taken care of, unimplemented! macro used to panic it it reaches that point
+        }
     }
 }
